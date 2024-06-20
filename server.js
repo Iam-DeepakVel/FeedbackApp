eb8c4d414f4a21496f520257297934318c06d7ec
Deepak <99487104+Iam-DeepakVel@users.noreply.github.com>
2024-06-20 10:56:31 +0000

    Add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import connectDB from "./db/connectDB.js";
 import { feedbackRoutes } from "./routes/feedbackRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/feedbacks", feedbackRoutes);
 
